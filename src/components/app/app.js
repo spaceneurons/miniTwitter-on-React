66663e66f6496afe1f03210f90fc149e50d887c4
Marogy <rushevamar@mail.ru>
2021-10-12 00:31:55 +0300

    добавили лайки и звездочки(избранное)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,19 +7,37 @@ import PostAddForm from "../post-add-form";
 
 import "./app.css";
 
+const App = () => {
+    // приложение APP на верхнем уровне получает какие -то данные передает их в
+    // компонент PostList компонент PostList формирует из этого посты на основании
+    // PostListItem
+    const data = [
+        {
+            label: 'Going to learn react',
+            important: true,
+            id:'asdasd'
+        }, {
+            label: 'Going to learn',
+            important: false,
+            id:'jhgghj'
+        }, {
+            label: 'Going to',
+            important: false,
+            id:'fffffg'
+        }
+    ];
 
-const App=()=>{
-    return(
+    return (
         <div className="app">
             <AppHeader/>
             <div className="seach-panel d-flex">
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
-          )
+    )
 }
 
 export default App;
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,24 +1,62 @@
-import React from "react";
+import React, {Component} from "react";
 import './post-list-item.css';
 
-const PostListItem=()=>{
-    return(
-        <li className="app-list-item d-flex justify-content-between">
-            <span className="app-list-item-label">
-                Hell
-            </span>
-            <div className="d-flex justtify-content-center align-items-center">
-                <button type="button" className="btn-star btn-sm">
-                    <i className="fa fa-star"></i>
-                </button>
-                <button  type="button" className="btn-trash btn-sm">
-                    <i className="fa fa-trash-o"></i>
-                </button>
-                <i className="fa fa-heart"></i>
+export default class PostListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            important: false,
+            like: false
+        };
+        //ПРИВЯЗВАЕМ к контексту!!!!!!!!!
+        this.onImportant=this.onImportant.bind(this);
+        this.onLike=this.onLike.bind(this);
+    }
+
+    // эо происходит когда мы кликаем на кнопку звездочки он работает с состоянием
+    // нашего компонента. Мы меняем important на противоположное значение
+    onImportant() {
+        this.setState(({important}) => ({
+            important: !important
+        }))
+    }
+
+    onLike() {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
+
+    render() {
+
+        const {label} = this.props;
+        const {important, like} =this.state;
+        let classNames = "app-list-item d-flex justify-content-between";
+
+        if (important) {
+            classNames += ' important';
+        }
+
+        if (like) {
+            classNames += ' like';
+        }
+
+        return (
+            <div className={classNames}>
+                <span className="app-list-item-label" onClick={this.onLike}>
+                    
+                    {label}
+                </span>
+                <div className="d-flex justtify-content-center align-items-center">
+                    <button type="button" className="btn-star btn-sm" onClick={this.onImportant}>
+                        <i className="fa fa-star"></i>
+                    </button>
+                    <button type="button" className="btn-trash btn-sm">
+                        <i className="fa fa-trash-o"></i>
+                    </button>
+                    <i className="fa fa-heart"></i>
+                </div>
             </div>
-        </li>
-    )
+        )
+    }
 }
-
-
-export default PostListItem;
